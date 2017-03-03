bc56ff848238f3aa78cdd1bc4a9c85ff7d03f51e
zyb <1140114702@qq.com>
2017-03-03 16:24:57 +0800

    Initial commit

---
+++ src/com/java/test/HelloWorld.java
@@ -0,0 +1,40 @@
+package com.java.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+/**
+ * Created by zyb on 2017/3/3.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        File file = new File("E:/javaSpace/word.txt");
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            byte[] bytes = "这是一段文字，这是一段文字。".getBytes();
+            out.write(bytes);
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            FileInputStream in = new FileInputStream(file);
+            byte[] bytes = new byte[1024];
+            int len = in.read(bytes);
+            System.out.print(new String(bytes,0,len));
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        if (file.exists()) {
+//            System.out.println(file.getName());
+//            System.out.println(file.length());
+//            System.out.println(file.isHidden());
+//        } else {
+//            System.out.println("文件已创建");
+//        }
+    }
+
+}
